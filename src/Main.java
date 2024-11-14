f47aff81d96c2b7ffd76ad9d2ccf393ff54697e3
dannamishelle23 <dannamishelle.23@gmail.com>
2024-11-13 23:28:14 -0500

    4to commit

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,24 @@ import java.util.InputMismatchException;
 //Ingreso de datos usando scanner
 import java.util.Scanner;
 
-//Definir la clase
+//Definir la clase principal
 public class Main {
     public static void main(String[] args) {
+
+        //Crear una instancia de la clase EJ5
         EJ5 golpeo = new EJ5();
         //Crear una instancia
         Scanner scanner = new Scanner(System.in);
 
         //Ingreso de la distancia con datos por teclado
+
+        //se usa try y catch para excepciones en caso de ingresar datos incorrectos
         try {
             System.out.print("Ingrese la distancia a la portería en metros: ");
             double distancia = scanner.nextDouble();
             if (distancia <= 0) {
                 System.out.println("Error! La distancia debe ser mayor o igual a cero.");
-                return;  //El programa termina si la distancia es negativa o cero
+                return;  //El programa termina cuando el usuario ingresa una distancia negativa o cero
             }
             golpeo.setDistancia(distancia);
         } catch (InputMismatchException e) {
@@ -33,7 +37,7 @@ public class Main {
                 return; //termina el programa si el ángulo es inválido
             }
             golpeo.setAngulo(angulo);
-        } catch (InputMismatchException e) {
+        } catch (InputMismatchException e) {         //en caso de que el usuario ingrese un dato erroneo
             System.out.println("Error! Debe ingresar un número válido para el ángulo.");
             return;
         }
@@ -52,16 +56,17 @@ public class Main {
             return;
         }
 
-        //Calcular la velocidad necesaria
+        //Calcular la velocidad necesaria. Llamar al metodo personalizado.
         double velocidadRequerida = golpeo.calcularVelocidad();
 
         // Mostrar los valores ingresados y el resultado
-        System.out.printf("Distancia: %.2f metros%n", golpeo.getDistancia()"metros.");
-        System.out.printf("Ángulo: %.2f grados%n", golpeo.getAngulo())"grados";
-        System.out.printf("Tiempo: %.2f segundos%n", golpeo.getTiempo()), "segundos";
+        System.out.printf("Distancia: %.2f metros%n", golpeo.getDistancia()"metros.");    //obtener la informacion de la distancia usando get
+        System.out.printf("Ángulo: %.2f grados%n", golpeo.getAngulo())"grados";       //obtener la informacion de los grados usando get
+        System.out.printf("Tiempo: %.2f segundos%n", golpeo.getTiempo()), "segundos";   //obtener la informacion del tiempo usando get
         //Mostrar resultado con 2 decimales usando printf
         System.out.printf("La velocidad necesaria para que el balón entre en la portería es: %.2f", velocidadRequerida, "m/s.");
 
+        //Cerrar el scanner
         scanner.close();
     }
 }
